096ec17a3fb2ade7482c9c9ebfde0beb1f6d15db
smalaca <ab13.krakow@gmail.com>
2024-12-27 14:31:17 +0100

    add table name over OutboxEvent

--- open-trainings/src/main/java/com/smalaca/opentrainings/infrastructure/eventregistry/jpa/OutboxEvent.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/infrastructure/eventregistry/jpa/OutboxEvent.java
@@ -4,13 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
 @Entity
 @Getter
+@Table(name = "OUTBOX_EVENTS")
 public class OutboxEvent {
     @Id
     @Column(name = "EVENT_ID")
