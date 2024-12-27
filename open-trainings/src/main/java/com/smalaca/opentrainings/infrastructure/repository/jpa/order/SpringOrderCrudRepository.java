32b36e40338b27bb4acd519aab1dca5df1be5a1e
smalaca <ab13.krakow@gmail.com>
2024-12-27 12:35:00 +0100

    rename SpringCrudRepository into SpringOrderCrudRepository

--- open-trainings/src/main/java/com/smalaca/opentrainings/infrastructure/repository/jpa/order/SpringOrderCrudRepository.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/infrastructure/repository/jpa/order/SpringOrderCrudRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface SpringCrudRepository extends CrudRepository<Order, UUID> {
+public interface SpringOrderCrudRepository extends CrudRepository<Order, UUID> {
 }
