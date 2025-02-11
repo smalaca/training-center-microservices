4844d9b2420dc4181f33c7e25436c9cb7e4b83b7
smalaca <ab13.krakow@gmail.com>
2025-02-11 22:42:29 +0100

    127 - remove unnecessary OfferAcceptanceSaga.isCompleted field

--- open-trainings/src/main/java/com/smalaca/opentrainings/domain/offeracceptancesaga/OfferAcceptanceSaga.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/domain/offeracceptancesaga/OfferAcceptanceSaga.java
@@ -8,18 +8,13 @@ import java.util.UUID;
 
 @Saga
 public class OfferAcceptanceSaga {
-    private static final Boolean COMPLETED = true;
-    private static final Boolean NOT_COMPLETED = false;
-
     private final UUID offerId;
-    private Boolean isCompleted = NOT_COMPLETED;
 
     public OfferAcceptanceSaga(UUID offerId) {
         this.offerId = offerId;
     }
 
     public AcceptOfferCommand accept(OfferAcceptanceRequestedEvent event) {
-        isCompleted = COMPLETED;
         return new AcceptOfferCommand(event.offerId(), event.firstName(), event.lastName(), event.email(), event.discountCode());
     }
 }
