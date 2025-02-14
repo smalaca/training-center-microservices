0f3fe5e474dbc4349fabdc22e6bd4018d92b42b8
smalaca <ab13.krakow@gmail.com>
2025-02-14 21:13:14 +0100

    REFACT - add missing @DomainEvent annotation over OfferAcceptanceRequestedEvent

--- open-trainings/src/main/java/com/smalaca/opentrainings/domain/offeracceptancesaga/events/OfferAcceptanceRequestedEvent.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/domain/offeracceptancesaga/events/OfferAcceptanceRequestedEvent.java
@@ -1,11 +1,13 @@
 package com.smalaca.opentrainings.domain.offeracceptancesaga.events;
 
+import com.smalaca.domaindrivendesign.DomainEvent;
 import com.smalaca.opentrainings.domain.eventid.EventId;
 import com.smalaca.opentrainings.domain.offeracceptancesaga.OfferAcceptanceSaga;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@DomainEvent
 public record OfferAcceptanceRequestedEvent(EventId eventId, UUID offerId, String firstName, String lastName, String email, String discountCode) implements OfferAcceptanceSagaEvent{
     public static OfferAcceptanceRequestedEvent create(UUID sagaId, String firstName, String lastName, String email, String discountCode) {
         return new OfferAcceptanceRequestedEvent(EventId.newEventId(), sagaId, firstName, lastName, email, discountCode);
