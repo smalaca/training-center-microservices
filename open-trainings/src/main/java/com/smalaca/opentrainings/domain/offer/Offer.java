bfefcc9bf01c1f917d1d647491435f3b75d80653
smalaca <ab13.krakow@gmail.com>
2025-02-16 09:41:43 +0100

    146 - move logic from AcceptOfferDomainCommand to Offer

--- open-trainings/src/main/java/com/smalaca/opentrainings/domain/offer/Offer.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/domain/offer/Offer.java
@@ -1,5 +1,6 @@
 package com.smalaca.opentrainings.domain.offer;
 
+import com.google.common.base.Strings;
 import com.smalaca.domaindrivendesign.AggregateRoot;
 import com.smalaca.domaindrivendesign.Factory;
 import com.smalaca.opentrainings.domain.clock.Clock;
@@ -10,6 +11,7 @@ import com.smalaca.opentrainings.domain.offer.commands.AcceptOfferDomainCommand;
 import com.smalaca.opentrainings.domain.offer.events.OfferEvent;
 import com.smalaca.opentrainings.domain.offer.events.OfferRejectedEvent;
 import com.smalaca.opentrainings.domain.personaldatamanagement.PersonalDataManagement;
+import com.smalaca.opentrainings.domain.personaldatamanagement.PersonalDataRequest;
 import com.smalaca.opentrainings.domain.personaldatamanagement.PersonalDataResponse;
 import com.smalaca.opentrainings.domain.price.Price;
 import com.smalaca.opentrainings.domain.trainingoffercatalogue.TrainingBookingDto;
@@ -87,7 +89,7 @@ public class Offer {
             return OfferRejectedEvent.expired(offerId);
         }
 
-        PersonalDataResponse response = personalDataManagement.save(command.asPersonalDataRequest());
+        PersonalDataResponse response = personalDataManagement.save(asPersonalDataRequest(command));
 
         if (response.isFailed()) {
             throw new MissingParticipantException();
@@ -114,8 +116,16 @@ public class Offer {
                 .build();
     }
 
+    private PersonalDataRequest asPersonalDataRequest(AcceptOfferDomainCommand command) {
+        return PersonalDataRequest.builder()
+                .firstName(command.firstName())
+                .lastName(command.lastName())
+                .email(command.email())
+                .build();
+    }
+
     private Price finalPrice(AcceptOfferDomainCommand command, DiscountService discountService, PersonalDataResponse response) {
-        if (command.hasNoDiscountCode()) {
+        if (hasNoDiscountCode(command)) {
             return trainingPrice;
         }
 
@@ -127,6 +137,10 @@ public class Offer {
         }
 
         return discount.newPrice();
+    }
+
+    private boolean hasNoDiscountCode(AcceptOfferDomainCommand command) {
+        return Strings.isNullOrEmpty(command.discountCode());
     }
 
     private boolean isOfferNotAvailable(Clock clock, TrainingOfferCatalogue trainingOfferCatalogue) {
--- open-trainings/src/main/java/com/smalaca/opentrainings/domain/offer/commands/AcceptOfferDomainCommand.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/domain/offer/commands/AcceptOfferDomainCommand.java
@@ -1,18 +1,4 @@
 package com.smalaca.opentrainings.domain.offer.commands;
 
-import com.google.common.base.Strings;
-import com.smalaca.opentrainings.domain.personaldatamanagement.PersonalDataRequest;
-
 public record AcceptOfferDomainCommand(String firstName, String lastName, String email, String discountCode) {
-    public PersonalDataRequest asPersonalDataRequest() {
-        return PersonalDataRequest.builder()
-                .firstName(firstName)
-                .lastName(lastName)
-                .email(email)
-                .build();
-    }
-
-    public boolean hasNoDiscountCode() {
-        return Strings.isNullOrEmpty(discountCode);
-    }
 }
