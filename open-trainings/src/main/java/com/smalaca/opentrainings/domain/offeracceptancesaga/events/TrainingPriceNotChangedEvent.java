70b059f5d904a10da36369ae68a071858a4b7a1a
smalaca <ab13.krakow@gmail.com>
2025-03-02 15:16:36 +0100

    REFACT - remove unused imports from TrainingPriceNotChangedEvent

--- open-trainings/src/main/java/com/smalaca/opentrainings/domain/offeracceptancesaga/events/TrainingPriceNotChangedEvent.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/domain/offeracceptancesaga/events/TrainingPriceNotChangedEvent.java
@@ -2,10 +2,8 @@ package com.smalaca.opentrainings.domain.offeracceptancesaga.events;
 
 import com.smalaca.opentrainings.domain.eventid.EventId;
 import com.smalaca.opentrainings.domain.offeracceptancesaga.OfferAcceptanceSaga;
-import com.smalaca.opentrainings.domain.offeracceptancesaga.commands.AcceptOfferCommand;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
 public record TrainingPriceNotChangedEvent(EventId eventId, UUID offerId, UUID trainingId) implements OfferAcceptanceSagaEvent {
