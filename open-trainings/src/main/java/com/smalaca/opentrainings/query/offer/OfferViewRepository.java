ab957227cf92a5610a60043a23003b350989fbb3
smalaca <ab13.krakow@gmail.com>
2025-06-21 09:39:59 +0200

    REFACT - decrease visibility of OfferViewRepository

--- open-trainings/src/main/java/com/smalaca/opentrainings/query/offer/OfferViewRepository.java
+++ open-trainings/src/main/java/com/smalaca/opentrainings/query/offer/OfferViewRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface OfferViewRepository extends CrudRepository<OfferView, UUID> {
+interface OfferViewRepository extends CrudRepository<OfferView, UUID> {
     @Query("SELECT o FROM OfferView o WHERE o.status = 'INITIATED' AND o.creationDateTime < :tenMinutesAgo")
     Iterable<OfferView> findAllInitiatedOffersOlderThan(@Param("tenMinutesAgo") LocalDateTime tenMinutesAgo);
 }
