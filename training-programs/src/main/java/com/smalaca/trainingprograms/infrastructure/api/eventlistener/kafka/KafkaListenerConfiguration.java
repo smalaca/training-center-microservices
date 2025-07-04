f209a8c0244642915e19f8f24dc1f5bb00695d05
smalaca <ab13.krakow@gmail.com>
2025-07-04 12:01:04 +0200

    321 - add missing KafkaListenerConfiguration to training program module

---
+++ training-programs/src/main/java/com/smalaca/trainingprograms/infrastructure/api/eventlistener/kafka/KafkaListenerConfiguration.java
@@ -0,0 +1,29 @@
+package com.smalaca.trainingprograms.infrastructure.api.eventlistener.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaListenerConfiguration {
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> listenerContainerFactory(@Value("${kafka.bootstrap-servers}") String bootstrapServers) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(JsonDeserializer.TRUSTED_PACKAGES, "com.smalaca.schemaregistry.*");
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(properties));
+        return factory;
+    }
+}
