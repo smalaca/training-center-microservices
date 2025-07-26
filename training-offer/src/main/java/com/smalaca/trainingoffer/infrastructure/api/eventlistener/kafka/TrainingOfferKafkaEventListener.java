671c85b39ad972345b307298bf28316e41c85bcb
smalaca <ab13.krakow@gmail.com>
2025-07-26 15:44:44 +0200

    397 [Training Offer][Publish] listen over BookTrainingPlaceCommand in TrainingOfferKafkaEventListener

--- training-offer/src/main/java/com/smalaca/trainingoffer/infrastructure/api/eventlistener/kafka/TrainingOfferKafkaEventListener.java
+++ training-offer/src/main/java/com/smalaca/trainingoffer/infrastructure/api/eventlistener/kafka/TrainingOfferKafkaEventListener.java
@@ -1,5 +1,6 @@
 package com.smalaca.trainingoffer.infrastructure.api.eventlistener.kafka;
 
+import com.smalaca.schemaregistry.trainingoffer.commands.BookTrainingPlaceCommand;
 import com.smalaca.schemaregistry.trainingoffer.commands.ConfirmTrainingPriceCommand;
 import com.smalaca.trainingoffer.application.trainingoffer.TrainingOfferApplicationService;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -21,6 +22,14 @@ public class TrainingOfferKafkaEventListener {
         trainingOfferApplicationService.confirmPrice(asConfirmTrainingPriceCommand(command));
     }
 
+    @KafkaListener(
+            topics = "${kafka.topics.trainingoffer.commands.book-training-place}",
+            groupId = "${kafka.group-id}",
+            containerFactory = "listenerContainerFactory")
+    public void listen(BookTrainingPlaceCommand command) {
+        trainingOfferApplicationService.book(asBookTrainingPlaceCommand(command));
+    }
+
     private com.smalaca.trainingoffer.domain.trainingoffer.commands.ConfirmTrainingPriceCommand asConfirmTrainingPriceCommand(ConfirmTrainingPriceCommand command) {
         return new com.smalaca.trainingoffer.domain.trainingoffer.commands.ConfirmTrainingPriceCommand(
                 toDomainCommandId(command.commandId()),
@@ -28,6 +37,14 @@ public class TrainingOfferKafkaEventListener {
                 command.trainingId(),
                 command.priceAmount(),
                 command.priceCurrencyCode());
+    }
+
+    private com.smalaca.trainingoffer.domain.trainingoffer.commands.BookTrainingPlaceCommand asBookTrainingPlaceCommand(BookTrainingPlaceCommand command) {
+        return new com.smalaca.trainingoffer.domain.trainingoffer.commands.BookTrainingPlaceCommand(
+                toDomainCommandId(command.commandId()),
+                command.offerId(),
+                command.participantId(),
+                command.trainingId());
     }
 
     private com.smalaca.trainingoffer.domain.commandid.CommandId toDomainCommandId(com.smalaca.schemaregistry.metadata.CommandId commandId) {
