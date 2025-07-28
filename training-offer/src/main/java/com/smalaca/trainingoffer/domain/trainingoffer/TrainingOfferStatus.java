343d78aca15b2663a618a061da3cffbdd7ca4afa
smalaca <ab13.krakow@gmail.com>
2025-07-28 16:07:19 +0200

    437 - [Training Offer] add value object over TrainingOfferStatus

--- training-offer/src/main/java/com/smalaca/trainingoffer/domain/trainingoffer/TrainingOfferStatus.java
+++ training-offer/src/main/java/com/smalaca/trainingoffer/domain/trainingoffer/TrainingOfferStatus.java
@@ -1,5 +1,8 @@
 package com.smalaca.trainingoffer.domain.trainingoffer;
 
+import com.smalaca.domaindrivendesign.ValueObject;
+
+@ValueObject
 enum TrainingOfferStatus {
     PUBLISHED, RESCHEDULED
 }
