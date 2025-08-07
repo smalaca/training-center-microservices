a838ca7cf44002d34f816c6bf62addd0e6a3317b
smalaca <ab13.krakow@gmail.com>
2025-08-07 13:20:33 +0200

    460 - [Reviews] ProposalReviewAssignmentPolicy - Add SpecializationAssignmentPolicy - increase descriptiveness of ReviewerAssignmentPolicyTest

--- reviews/src/test/java/com/smalaca/reviews/domain/proposal/ReviewerAssignmentPolicyTest.java
+++ reviews/src/test/java/com/smalaca/reviews/domain/proposal/ReviewerAssignmentPolicyTest.java
@@ -18,6 +18,10 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 class ReviewerAssignmentPolicyTest {
+    private static final UUID CATEGORY_ONE = randomUUID();
+    private static final UUID CATEGORY_TWO = randomUUID();
+    private static final UUID CATEGORY_THREE = randomUUID();
+    private static final Set<UUID> PROPOSAL_CATEGORIES = Set.of(CATEGORY_ONE, CATEGORY_TWO, CATEGORY_THREE);
     private static final UUID AUTHOR_ID = randomUUID();
     private static final LocalDateTime NOW = LocalDateTime.now();
     
@@ -33,126 +37,96 @@ class ReviewerAssignmentPolicyTest {
     @Test
     void noAssignmentPolicyShouldAlwaysReturnQueuedAssignmentWithNullReviewer() {
         given(trainersCatalogue.findAllTrainers()).willReturn(emptyList());
-        Set<UUID> categories = Set.of(randomUUID(), randomUUID());
 
-        Assignment assignment = policy.assign(AUTHOR_ID, categories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
         assertThatAssignment(assignment)
-                .hasNoReviewerId()
                 .isQueued()
+                .hasNoReviewerId()
                 .hasOccurredAt(NOW);
     }
 
     @Test
     void specializationAssignmentPolicyShouldAssignTrainerWhoSupportsAllCategories() {
-        UUID trainerId = randomUUID();
-        UUID category1 = randomUUID();
-        UUID category2 = randomUUID();
-        Set<UUID> requiredCategories = Set.of(category1, category2);
-        
-        Trainer perfectTrainer = new Trainer(trainerId, Set.of(category1, category2, randomUUID()));
+        Trainer perfectTrainer = trainer(CATEGORY_ONE, CATEGORY_TWO, CATEGORY_THREE, randomUUID());
         given(trainersCatalogue.findAllTrainers()).willReturn(List.of(perfectTrainer));
         
-        Assignment assignment = policy.assign(AUTHOR_ID, requiredCategories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
         assertThatAssignment(assignment)
-                .hasReviewerId(trainerId)
                 .isAssigned()
+                .hasReviewerId(perfectTrainer.id())
                 .hasOccurredAt(NOW);
     }
 
     @Test
     void specializationAssignmentPolicyShouldAssignBestPartialMatchWhenNoPerfectMatch() {
-        UUID trainerId1 = randomUUID();
-        UUID trainerId2 = randomUUID();
-        UUID category1 = randomUUID();
-        UUID category2 = randomUUID();
-        UUID category3 = randomUUID();
-        Set<UUID> requiredCategories = Set.of(category1, category2, category3);
+        Trainer partialTrainerOne = trainer(CATEGORY_ONE, randomUUID());
+        Trainer partialTrainerTwo = trainer(randomUUID(), CATEGORY_ONE, CATEGORY_TWO, randomUUID());
+        given(trainersCatalogue.findAllTrainers()).willReturn(List.of(partialTrainerOne, partialTrainerTwo));
         
-        Trainer partialTrainer1 = new Trainer(trainerId1, Set.of(category1)); // matches 1
-        Trainer partialTrainer2 = new Trainer(trainerId2, Set.of(category1, category2)); // matches 2 - best
-        given(trainersCatalogue.findAllTrainers()).willReturn(List.of(partialTrainer1, partialTrainer2));
-        
-        Assignment assignment = policy.assign(AUTHOR_ID, requiredCategories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
         assertThatAssignment(assignment)
-                .hasReviewerId(trainerId2)
                 .isAssigned()
+                .hasReviewerId(partialTrainerTwo.id())
                 .hasOccurredAt(NOW);
     }
 
     @Test
     void specializationAssignmentPolicyShouldAssignFirstTrainerWhenMultipleHaveSamePartialMatch() {
-        UUID trainerId1 = randomUUID();
-        UUID trainerId2 = randomUUID();
-        UUID category1 = randomUUID();
-        UUID category2 = randomUUID();
-        UUID category3 = randomUUID();
-        Set<UUID> requiredCategories = Set.of(category1, category2, category3);
+        Trainer partialTrainerOne = trainer(CATEGORY_ONE, randomUUID());
+        Trainer partialTrainerTwo = trainer(CATEGORY_TWO, randomUUID());
+        given(trainersCatalogue.findAllTrainers()).willReturn(List.of(partialTrainerOne, partialTrainerTwo));
         
-        Trainer partialTrainer1 = new Trainer(trainerId1, Set.of(category1, category2)); // matches 2 - first
-        Trainer partialTrainer2 = new Trainer(trainerId2, Set.of(category2, category3)); // matches 2 - second
-        given(trainersCatalogue.findAllTrainers()).willReturn(List.of(partialTrainer1, partialTrainer2));
-        
-        Assignment assignment = policy.assign(AUTHOR_ID, requiredCategories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
         assertThatAssignment(assignment)
-                .hasReviewerId(trainerId1)
                 .isAssigned()
+                .hasReviewerId(partialTrainerOne.id())
                 .hasOccurredAt(NOW);
     }
 
     @Test
     void specializationAssignmentPolicyShouldDelegateToFallbackWhenNoTrainerSupportsAnyCategory() {
-        UUID category1 = randomUUID();
-        UUID category2 = randomUUID();
-        Set<UUID> requiredCategories = Set.of(category1, category2);
-        
-        Trainer unrelatedTrainer = new Trainer(randomUUID(), Set.of(randomUUID(), randomUUID()));
+        Trainer unrelatedTrainer = trainer(randomUUID(), randomUUID());
         given(trainersCatalogue.findAllTrainers()).willReturn(List.of(unrelatedTrainer));
         
-        Assignment assignment = policy.assign(AUTHOR_ID, requiredCategories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
-        // Should delegate to NoAssignmentPolicy
         assertThatAssignment(assignment)
-                .hasNoReviewerId()
                 .isQueued()
+                .hasNoReviewerId()
                 .hasOccurredAt(NOW);
     }
 
     @Test
     void specializationAssignmentPolicyShouldDelegateToFallbackWhenNoTrainersExist() {
-        Set<UUID> requiredCategories = Set.of(randomUUID(), randomUUID());
-        
         given(trainersCatalogue.findAllTrainers()).willReturn(List.of());
         
-        Assignment assignment = policy.assign(AUTHOR_ID, requiredCategories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
-        // Should delegate to NoAssignmentPolicy
         assertThatAssignment(assignment)
-                .hasNoReviewerId()
                 .isQueued()
+                .hasNoReviewerId()
                 .hasOccurredAt(NOW);
     }
 
     @Test
     void specializationAssignmentPolicyShouldPreferPerfectMatchOverPartialMatch() {
-        UUID perfectTrainerId = randomUUID();
-        UUID partialTrainerId = randomUUID();
-        UUID category1 = randomUUID();
-        UUID category2 = randomUUID();
-        Set<UUID> requiredCategories = Set.of(category1, category2);
-        
-        Trainer partialTrainer = new Trainer(partialTrainerId, Set.of(category1, randomUUID(), randomUUID()));
-        Trainer perfectTrainer = new Trainer(perfectTrainerId, Set.of(category1, category2));
+        Trainer partialTrainer = trainer(CATEGORY_ONE, randomUUID(), randomUUID());
+        Trainer perfectTrainer = trainer(CATEGORY_ONE, CATEGORY_TWO, CATEGORY_THREE);
         given(trainersCatalogue.findAllTrainers()).willReturn(List.of(partialTrainer, perfectTrainer));
         
-        Assignment assignment = policy.assign(AUTHOR_ID, requiredCategories);
+        Assignment assignment = policy.assign(AUTHOR_ID, PROPOSAL_CATEGORIES);
 
         assertThatAssignment(assignment)
-                .hasReviewerId(perfectTrainerId)
                 .isAssigned()
+                .hasReviewerId(perfectTrainer.id())
                 .hasOccurredAt(NOW);
+    }
+
+    private Trainer trainer(UUID... categories) {
+        return new Trainer(randomUUID(), Set.of(categories));
     }
 }
