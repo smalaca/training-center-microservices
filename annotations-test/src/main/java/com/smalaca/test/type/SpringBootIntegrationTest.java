8e3dccf30273e9fc0b20a8a58d6d4d013a87fb8b
smalaca <ab13.krakow@gmail.com>
2025-10-11 18:59:55 +0200

    FIX - add dirties context to SpringBootIntegrationTest and SystemTest annotation

--- annotations-test/src/main/java/com/smalaca/test/type/SpringBootIntegrationTest.java
+++ annotations-test/src/main/java/com/smalaca/test/type/SpringBootIntegrationTest.java
@@ -1,15 +1,19 @@
 package com.smalaca.test.type;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @IntegrationTest
+@DirtiesContext(classMode = AFTER_CLASS)
 @SpringBootTest
 public @interface SpringBootIntegrationTest {
 }
